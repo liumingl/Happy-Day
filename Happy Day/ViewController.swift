f43499c50f6c7ff3cb4926e7655670e405aa5695
铭刘 <liuming@mingliudemacbook-pro.local>
2016-06-19 09:36:30 +0800

    Init

---
+++ Happy Day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happy Day
+//
+//  Created by 铭刘 on 16/6/19.
+//  Copyright © 2016年 铭刘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
